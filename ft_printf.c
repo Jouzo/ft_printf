a91fef9ac0c69f02f5d10472e53cf73e866caac4
Jeremy D'ESCLERCS <jdescler@e3r2p9.42.fr>
2019-01-21 12:52:15 +0100

    voili

---
+++ ft_printf.c
@@ -0,0 +1,17 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+
+int ft_printf(const char *format, ...)
+{
+    va_list ap;
+
+    va_start(ap, format);
+
+    while (*format)
+    {
+
+         va_arg(ap, int);
+    }
+    va_end(ap);
+}
---
+++ parsing.c
@@ -0,0 +1,15 @@
+int     count_args(char *str)
+{
+    int i;
+
+    i = 0;
+    while (*str)
+    {
+        if (*str == "%")
+        {
+            i++;
+        }
+        *str++;
+    }
+    return (i);
+}
