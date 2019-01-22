3f40b16c5bb85203d8a0dc94e96ffcee369419a9
Massoud MOVAHHED <mmovahhe@e2r10p14.42.fr>
2019-01-22 14:20:55 +0100

    de

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -22,17 +22,19 @@ typedef struct      s_list
     unsigned int    is_long_double : 1;     /* L flag.  */
     unsigned int    is_short : 1;           /* h flag.  */
     unsigned int    is_long : 1;            /* l flag.  */
+    unsigned int    is_long_long : 1;            /* ll flag.  */
+    unsigned int    is_char : 1;            /* hh flag.  */
     unsigned int    alt : 1;                /* # flag.  */
     unsigned int    space : 1;              /* Space flag.  */
     unsigned int    left : 1;               /* - flag.  */
     unsigned int    showsign : 1;           /* + flag.  */
+    unsigned int    zero : 1;           /* 0 flag.  */
     unsigned int    group : 1;              /* ' flag.  */
     unsigned int    extra : 1;              /* For special use.  */
-    unsigned int    is_char : 1;            /* hh flag.  */
     unsigned int    wide : 1;               /* Nonzero for wide character streams.  */
     unsigned int    i18n : 1;               /* I flag.  */
     unsigned int    is_binary128 : 1;       /* Floating-point argument is ABI-compatible */
-    char            pad;                     /* Padding character.  */
+    // char            pad;                     /* Padding character.  */
     struct s_list   *next;
 }                   t_list;
 
