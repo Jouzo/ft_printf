d55eba4cd6ae579b7c4fea205617d116340e05e2
Jeremy D'ESCLERCS <jdescler@e2r10p12.42.fr>
2019-01-22 11:50:44 +0100

    struct + header

---
+++ includes/ft_printf.h
@@ -0,0 +1,45 @@
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+#include "../libft/libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+** Macros
+*/
+
+/*
+** Structures
+*/
+
+typedef struct      s_list
+{
+    int             prec;                        /* Precision. */
+    int             width;                       /* Width.  */
+    char            spec;                    /* Format letter.  */
+    unsigned int    is_long_double : 1;     /* L flag.  */
+    unsigned int    is_short : 1;           /* h flag.  */
+    unsigned int    is_long : 1;            /* l flag.  */
+    unsigned int    alt : 1;                /* # flag.  */
+    unsigned int    space : 1;              /* Space flag.  */
+    unsigned int    left : 1;               /* - flag.  */
+    unsigned int    showsign : 1;           /* + flag.  */
+    unsigned int    group : 1;              /* ' flag.  */
+    unsigned int    extra : 1;              /* For special use.  */
+    unsigned int    is_char : 1;            /* hh flag.  */
+    unsigned int    wide : 1;               /* Nonzero for wide character streams.  */
+    unsigned int    i18n : 1;               /* I flag.  */
+    unsigned int    is_binary128 : 1;       /* Floating-point argument is ABI-compatible */
+    char            pad;                     /* Padding character.  */
+    struct s_list   *next;
+}                   t_list;
+
+/*
+** Functions
+*/
+
+
+
+#endif
