a465803549346ba1bd4e4edcb7b77d04174ad440
Jeremy D'ESCLERCS <jdescler@e2r10p12.42.fr>
2019-01-22 14:20:04 +0100

    strcpy

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 int     main()
 {
-    printf("%+# 8d", 45);
+    char *format;
+    char *format2;
+    format = "23";
+    format2 = "salut les %d gros come state ragga";
+    char ret[10000];
+
+    strcpy(ret, format2);
+    strcpy(ret + 10, format);
+    strcpy(ret+10 + strlen(format), format2+10 +strlen(format));
+    printf("%s", ret);
+    // printf("%.4d", 45);
     return (0);
 }
