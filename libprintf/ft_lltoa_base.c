859d54bfde7da138c4c3b1a9cca991ad325dfe8b
Massoud MOVAHHED <mmovahhe@e3r8p17.42.fr>
2019-01-24 17:47:27 +0100

    avec le itoa fonctionnel mais moche

--- libprintf/ft_lltoa_base.c
+++ libprintf/ft_lltoa_base.c
@@ -3,30 +3,23 @@
 #include "../includes/ft_printf.h"
 void    ft_itoa_base_b(int nb, int base, char **res)
 {
-    char *str_base = "0123456789abcdef0123456789ABCDEF";
-
+    char *str_base;
     int i;
     int capitale = 0; // a rajouter dans la struct
-    int tmp;
+
+    str_base = "0123456789abcdef0123456789ABCDEF";
     char buf[] = "000000000000000000";
     char *buf_bis;
 
-    tmp = 0;
     i = 0;
     while (nb > 0)
     {
-    //    printf("value tmp %c\n", str_base[nb % 10]);
-        tmp = str_base[nb % 10];
-        ft_memset(buf + i, tmp, 1);
-    //    *res[i] = 'a';
+        ft_memset(buf + i, str_base[nb % 10], 1);
         nb /= 10;
         i++;
-        printf("value de res: %s\n", buf);
     }
     buf[i] = '\0';
     ft_memcpy(buf_bis, buf, ft_strlen(buf) + 1);
- //   printf("dede%s\n", *res);
-    printf("value de buf_bis: %s\n", buf_bis);
     *res = buf_bis;
 }
 
@@ -34,6 +27,6 @@ int main()
 {
     char *res;
     
-    ft_itoa_base_b(2147483647, 10, &res);
+    ft_itoa_base_b(247, 10, &res);
     printf("%s", res);
 }
