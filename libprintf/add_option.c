e65a9c3f8d0cea29e1765d584fd18f36a93a4250
Jeremy D'ESCLERCS <jdescler@e3r11p11.42.fr>
2019-02-14 12:51:12 +0100

    norme add option

--- libprintf/add_option.c
+++ libprintf/add_option.c
@@ -48,88 +48,17 @@ void	fill_zero(char *buf, t_args *args, int size, int *p_buf)
 	}
 }
 
-void	width_over_prec(char *buf, t_args *args, int size, int *p_buf)
-{
-	int i;
-	int min;
-	int len;
-
-	i = 0;
-	min = 0;
-	if (args->prec <= size && (args->spec == 'd' || args->spec == 'o') && args->prec != -1)
-		args->prec = 0;
-	len = args->prec < size ? size : args->prec;
-	if (args->showsign || args->minus || args->space)
-		min = 1;
-	if (args->alt == 1 && (args->spec == 'x' || args->spec == 'o' || args->spec == 'p'))
-		min += args->spec == 'o' ? 1 : 2;
-	if (args->width - len - min >= 0 && !args->left)
-	{
-		if (args->width - len - min > BUFF_SIZE * i)
-			i += big_fill_prec(buf, p_buf, args, args->width - len - min);
-		ft_memset(buf + *p_buf, ' ', args->width - len - min - BUFF_SIZE * i);
-		*p_buf += args->width - len - min - BUFF_SIZE * i;
-	}
-	if (args->alt == 1 && (args->spec == 'x' || args->spec == 'o' || args->spec == 'p'))
-		add_hash(buf, args, p_buf);
-	if (args->showsign && !args->minus && args->spec == 'd')
-		print_sign(buf, p_buf, args);
-	if (args->minus)
-		print_minus(buf, p_buf, args);
-	if (args->prec - size > 0)
-	{
-		if (args->prec - size > BUFF_SIZE * i)
-			i += big_fill_prec(buf, p_buf, args, args->prec - size);
-		ft_memset(buf + *p_buf, '0', args->prec - size - BUFF_SIZE * i);
-		*p_buf += args->prec - size - BUFF_SIZE * i;
-	}
-}
-
-void	fill_prec(char *buf, t_args *args, int size, int *p_buf)
-{
-	int i;
-	int min;
-
-	i = 0;
-	min = 0;
-	if (args->prec <= size && (args->spec == 'd' || args->spec == 'o') && args->prec != -1)
-		args->prec = 0;
-	if (args->showsign || args->minus || args->space)
-		min = 1;
-	if (args->spec == 'o' && args->alt && args->prec > size)
-		args->prec -= 1;
-	if (args->prec - size > 0)
-	{
-		if (args->width - args->prec - min >= 0 && !args->left)
-		{
-			i = big_fill_prec(buf, p_buf, args, args->width - args->prec - min);
-			ft_memset(buf + *p_buf, ' ', args->width - args->prec - min - BUFF_SIZE * i);
-			*p_buf += args->width - args->prec - min - BUFF_SIZE * i;
-		}
-		if (args->showsign && !args->minus && args->spec == 'd')
-			print_sign(buf, p_buf, args);
-		if (args->prec - size > BUFF_SIZE * i)
-			i += big_fill_prec(buf, p_buf, args, args->prec - size);
-		ft_memset(buf + *p_buf, '0', args->prec - size - BUFF_SIZE * i);
-		*p_buf += args->prec - size - BUFF_SIZE * i;
-	}
-	else if (!args->zero)
-		padding_left(buf, args, args->width + size, p_buf);
-	else
-		padding_left(buf, args, size, p_buf);
-}
-
-void add_option(char *buf, t_args *args, char *conv, int *p_buf)
+void	add_option(char *buf, t_args *args, char *conv, int *p_buf)
 {
 	if (((args->space && args->width && !args->left && !args->zero)
 		|| (args->width && !args->zero && !args->left)) && args->prec == 0)
 		padding_left(buf, args, ft_strlen(conv), p_buf);
 	if (args->space && !args->showsign && args->spec == 'd' && !args->minus)
 		one_space(buf, p_buf, args);
-	if (args->alt == 1 && (args->spec == 'x' || args->spec == 'o'
-	|| args->spec == 'p') && !(args->prec != 0 && args->prec < args->width))
+	if (args->alt == 1 && !(args->prec != 0 && args->prec < args->width))
 		add_hash(buf, args, p_buf);
-	if (args->minus && ((args->prec >= args->width) || (args->width && !args->prec)))
+	if (args->minus && ((args->prec >= args->width)
+		|| (args->width && !args->prec)))
 		print_minus(buf, p_buf, args);
 	if (args->showsign && !args->minus && args->spec == 'd'
 		&& args->prec >= args->width)
