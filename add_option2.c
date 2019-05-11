79d3885597fbcdeeab6eb5dd34297e9c94575e95
Massoud MOVAHHED <mmovahhe@e2r12p18.42.fr>
2019-05-11 16:54:26 +0200

    oupsi

--- add_option2.c
+++ add_option2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   add_option2.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdescler <jdescler@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mmovahhe <mmovahhe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 18:41:38 by mmovahhe          #+#    #+#             */
-/*   Updated: 2019/05/11 14:56:24 by jdescler         ###   ########.fr       */
+/*   Updated: 2019/05/11 16:53:11 by mmovahhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,8 @@ void	padding_right(char *buf, char *conv, t_args *args, int *p_buf)
 
 	i = 0;
 	len = args->alt ? ft_strlen(conv) + 1 : ft_strlen(conv);
-	if (args->showsign || args->minus || args->space
-		|| (args->alt && (args->spec == 'x' || args->spec == 'p')))
-		len++;
+	args->showsign || args->minus || args->space || (args->alt
+		&& (args->spec == 'x' || args->spec == 'p')) ? len++ : 0;
 	if (len - args->alt < args->prec && args->prec)
 	{
 		if (args->spec == 'o')
@@ -29,13 +28,15 @@ void	padding_right(char *buf, char *conv, t_args *args, int *p_buf)
 		else
 			len = args->prec + args->alt * 2;
 	}
+	args->left && args->showsign &&
+		args->width > args->prec && args->prec > 0 ? len++ : 0;
 	if (args->width - len > 0)
 	{
 		if (args->width - len > BUFF_SIZE)
 			i = big_padding_right(buf + ft_strlen(conv),
 			p_buf, args, args->width - len);
-		ft_memset(buf + *p_buf + ft_strlen(conv),
-			' ', args->width - len - BUFF_SIZE * i);
+		ft_memset(buf + *p_buf + ft_strlen(conv), ' ',
+			args->width - len - BUFF_SIZE * i);
 		*p_buf += args->width - len - BUFF_SIZE * i;
 	}
 }
